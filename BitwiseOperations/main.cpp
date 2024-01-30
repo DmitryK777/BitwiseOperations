fec9eac0dd495589ab641678234f38a855f3293c
DmitryK777 <kislyakovdmv@gmail.com>
2024-01-30 21:30:12 +0300

    Done

---
+++ BitwiseOperations/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int firstVariable;
+	int secondVariable;
+
+	cout << "Введите первую переменную: "; cin >> firstVariable;
+	cout << "Введите вторую переменную: "; cin >> secondVariable;
+	cout << endl;
+
+	cout << "Вы ввели: " << endl;
+	cout << "first variable = " << firstVariable << " и " << "second variable = " << secondVariable << endl;
+	cout << endl;
+
+	firstVariable ^= secondVariable;
+	secondVariable ^= firstVariable;
+	firstVariable ^= secondVariable;
+
+	cout << "Поменяли значения местами: " << endl;
+	cout << "first variable = " << firstVariable << " и " << "second variable = " << secondVariable << endl;
+	cout << endl;
+}
